9390c6ada877c4a75794fcf02eeb37fe123a8998
mgalalm <squareno2@gmail.com>
2018-11-27 11:04:36 +0000

    Attempt to move the xdebug

--- bin/util/xdebug.sh
+++ bin/util/xdebug.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 OUT_PREFIX=$1
-wget http://xdebug.org/files/xdebug-2.6.1.tgz
+
+source $(dirname $BASH_SOURCE)/../../_util/include/manifest.sh
+dep_formula=${0#$WORKSPACE_DIR/}
+dep_name=$(basename $BASH_SOURCE)
+dep_version=${dep_formula##*"/${dep_name}-"}
+dep_package=ext-${dep_name}-${dep_version}
+series=7.1
+dep_manifest=${dep_package}_php-$series.composer.json
+dep_url=http://xdebug.org/files/xdebug-2.6.1.tgz
+wget $dep_url
 tar -xvzf xdebug-2.6.1.tgz
 cd xdebug-2.6.1
 phpize
@@ -20,3 +29,14 @@ xdebug.remote_port=9001
 xdebug.remote_connect_back=1
 EOF
 
+
+
+MANIFEST_REQUIRE="${MANIFEST_REQUIRE:-"{\"heroku-sys/php\":\"${series}.*\"}"}"
+MANIFEST_CONFLICT="${MANIFEST_CONFLICT:-"{\"heroku-sys/hhvm\":\"*\"}"}"
+MANIFEST_REPLACE="${MANIFEST_REPLACE:-"{}"}"
+MANIFEST_PROVIDE="${MANIFEST_PROVIDE:-"{}"}"
+MANIFEST_EXTRA="${MANIFEST_EXTRA:-"{\"config\":\"etc/php/conf.d/newrelic.ini-dist\",\"profile\":\"bin/profile.newrelic.sh\"}"}"
+
+python $(dirname $BASH_SOURCE)/../../_util/include/manifest.py "heroku-sys-php-extension" "heroku-sys/ext-${dep_name}" "$dep_version" "${dep_formula}.tar.gz" "$MANIFEST_REQUIRE" "$MANIFEST_CONFLICT" "$MANIFEST_REPLACE" "$MANIFEST_PROVIDE" "$MANIFEST_EXTRA" > $dep_manifest
+
+print_or_export_manifest_cmd "$(generate_manifest_cmd "$dep_manifest")"
