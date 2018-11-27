bf850f349b4b3f090600d380c2015ef40d94f19b
mgalalm <squareno2@gmail.com>
2018-11-27 15:09:54 +0000

    Try to override the default opcahce.ini

--- bin/util/opcache.sh
+++ bin/util/opcache.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-yes | cp $bp_dir/support/_conf/php/conf.d/010-ext-zend_opcache.ini $build_dir/.heroku/php/etc/php/conf.d/
+
+cat > $build_dir/.heroku/php/etc/php/conf.d/010-ext-zend_opcache.ini <<'EOF'
+zend_extension=opcache.so
+opcache.enable=0
+opcache.validate_timestamps=0
+opcache.fast_shutdown=0
+opcache.memory_consumption=256
+opcache.interned_strings_buffer=8
+opcache.max_accelerated_files=4000
+EOF
