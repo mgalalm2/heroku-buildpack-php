aad61ec5edd3def97c7125518f442dd80dbe6d3c
mgalalm <squareno2@gmail.com>
2018-11-27 13:53:21 +0000

    Revert back

--- bin/util/xdebug.sh
+++
@@ -1,5 +0,0 @@
- #!/usr/bin/env bash
- install_xdebug_ext() {
- $(which composer) require -d "$build_dir/.heroku/php" -- "heroku-sys/ext-xdebug:*" >> $build_dir/.heroku/php/install.log
- echo "- Xdebug detected, installed ext-xdebug" | indent
-}
