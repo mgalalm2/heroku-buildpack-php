278eeee1d4264064dfe219dcad7cf7632ff84021
mgalalm <squareno2@gmail.com>
2018-11-27 13:13:02 +0000

    Fix sytax error

--- bin/util/xdebug.sh
+++ bin/util/xdebug.sh
@@ -1,5 +1,5 @@
  #!/usr/bin/env bash
  install_xdebug_ext() {
- $(which composer) require --update-no-dev -d "$build_dir/.heroku/php" -- "heroku-sys/ext-xdebug:*" >> $build_dir/.heroku/php/install.log 2>&1;
+ $(which composer) require -d "$build_dir/.heroku/php" -- "heroku-sys/ext-xdebug:*" >> $build_dir/.heroku/php/install.log
  echo "- Xdebug detected, installed ext-xdebug" | indent
 }
